9ebcc8f0f9f7e910de45d7963708012221429123
waldronmatt <mattwaldron@ymail.com>
2021-08-08 12:39:14 -0400

    spacing

--- webpack.dev.js
+++ webpack.dev.js
@@ -14,7 +14,7 @@ const developmentConfig = {
     },
     index: 'index.html',
     port: 8001,
-    //writeToDisk: true,
+    // writeToDisk: true,
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
 };
